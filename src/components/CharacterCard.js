75f955a595cd133a25f2af30daf1a11be0c8c36b
devin-bielejec <devin.bieleje@gmail.com>
2019-08-23 11:34:16 -0400

    Cards are displayed correctly with appropriate information, skipping styling for now

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function CharacterCard(props) {
-  return <span>todo: character</span>;
+  return (
+    <div key={props.charInfo.id}>
+      <img src={props.charInfo.image}/>
+      <h3>{props.charInfo.name}</h3>
+      <h5>{props.charInfo.species} {props.charInfo.status}</h5>
+      <p>Location: {props.charInfo.location.name}</p>
+      <p>Origin: {props.charInfo.origin.name}</p>
+      <h4>Episodes</h4>
+    </div>
+  );
 }
