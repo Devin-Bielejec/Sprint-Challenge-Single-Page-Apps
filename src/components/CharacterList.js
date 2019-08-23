53cb251a5b4da8fb5a646f32661cc36dfc71c05b
devin-bielejec <devin.bieleje@gmail.com>
2019-08-23 11:29:43 -0400

    Done with character list, passing props down to character card

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -4,13 +4,14 @@ import axios from "axios";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
-  const [characters, setCharacters] = useState({});
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     axios.get("https://rickandmortyapi.com/api/character/")
     .then( res => {
       console.log(res);
+      setCharacters(res.data.results);
     })
     .catch( err => console.log("Error:", err));
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
@@ -18,6 +19,7 @@ export default function CharacterList() {
 
   return (
     <section className="character-list grid-view">
+      {characters.map( charInfo => <CharacterCard charInfo={charInfo}/>)}
       <h2>TODO: `array.map()` over your state here!</h2>
     </section>
   );
